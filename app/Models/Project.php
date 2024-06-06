408090208c4fc94d4ea32025116c5d70b33c3eec
gianlucafalaschi <148443246+gianlucafalaschi@users.noreply.github.com>
2024-06-06 19:55:45 +0200

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;  // questo non e' necessario, lo importiamo solo se vogliamo essere espliciti
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','slug','client_name', 'summary', 'cover_image', 'type_id'];
+
+    public function type() {
+         // la foreign key fa capo alla tabella Type il cui model e' Type 
+        return $this->belongsTo(Type::class); 
+    }
+
+}
