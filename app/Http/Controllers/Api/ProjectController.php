2ed1caf9c164e4e16342826784ee2d853e4f30f7
gianlucafalaschi <gian100gian@outlook.it>
2024-06-11 07:44:24 +0200

    add type and technologies to projects api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,8 +9,13 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
-        
+        //$projects = Project::all();
+        /* qui oltre a prendere tutti dati dal model project, 
+        gli dico di aggiungere type e technologies, le funzioni che ho nel 
+        model Project per i dati delle altre tabelle fuori da projects */
+        $projects = Project::with('type','technologies')->get();
+
+
         return response()->json([
             'success' => true,
             'results' => $projects
